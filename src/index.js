1bb2339121827785c3b6e7ecde979550c9b17fbf
devin-parker <39925615+devin-parker@users.noreply.github.com>
2019-01-31 13:29:13 -0600

    Update index.js
    updated with error handling

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const qs = require('querystring');
 const ticket = require('./ticket');
+const renameTicket = require('./renameTicket');
 const signature = require('./verifySignature');
 const debug = require('debug')('slash-command-template:index');
 const { WebClient } = require('@slack/client');
 const token = process.env.SLACK_CHANNEL_TOKEN;
 const web = new WebClient(token);
+const https = require('https');
+const fs = require('fs');
+const credentials = {
+  key: fs.readFileSync(__dirname + 'xxxx', 'utf8'),
+  cert: fs.readFileSync(__dirname + 'xxxx', 'utf8'),
+  ca: [fs.readFileSync(__dirname + '/1xxxx'),fs.readFileSync(__dirname + '/2xxxx'),fs.readFileSync(__dirname + '/3xxxx')]
+};
 
 const apiUrl = 'https://slack.com/api';
 
@@ -36,9 +44,10 @@ app.get('/', (req, res) => {
 });
 
 /*
- * Endpoint to receive /helpdesk slash command from Slack.
+ * Endpoint to receive /channel slash command from Slack.
  * Checks verification token and opens a dialog to capture more info.
  */
+
 app.post('/command', (req, res) => {
   // extract the slash command text, and trigger ID from payload
   const { text, trigger_id } = req.body;
@@ -47,15 +56,25 @@ app.post('/command', (req, res) => {
   if (signature.isVerified(req)) {
     // create the dialog payload - includes the dialog structure, Slack API token,
     // and trigger ID
+      //Creator or Rename checker
+    if (text.includes('rename')) {
+      var renameOrCreator = 'rename-channel';
+      var title = 'Rename a channel!';
+      var inputName = '';
+    }else{
+      var renameOrCreator = 'submit-ticket';
+      var title = 'Create a channel!';
+      var inputName = text;
+    };
     const dialog = {
       token: process.env.SLACK_ACCESS_TOKEN,
       trigger_id,
       dialog: JSON.stringify({
-        title: 'Create a channel!',
-        callback_id: 'submit-ticket',
+        title: title,
+        callback_id: renameOrCreator,
         submit_label: 'Submit',
         elements: [{
-            label: 'Pre-Fix',
+            label: 'Prefix',
             type: 'select',
             name: 'prefix',
             optional: false,
@@ -70,188 +89,6 @@ app.post('/command', (req, res) => {
                           ]
                       },
     {
-            "label": "Support",
-            "options": [
-                    {
-                        "label": "General Support",
-                        "value": "sup"
-                    },
-                    {
-                        "label": "Support Cases",
-                        "value": "sup_case"
-                    },
-            ]
-        },
-        {
-            "label": "Sales/Field",
-            "options": [
-                {
-                    "label": "Sales",
-                    "value": "sales"
-                },
-                {
-                    "label": "FCE",
-                    "value": "fce"
-                }
-            ]
-        },
-        {
-                "label": "Marketing",
-                "options": [
-                    {
-                        "label": "Marketing",
-                        "value": "mrkt"
-                    }
-                ]
-            },
-            {
-                "label": "HR",
-                "options": [
-                    {
-                        "label": "HR",
-                        "value": "hr"
-                    }
-                ]
-            },
-            {
-                "label": "Projects",
-                "options": [
-                    {
-                        "label": "Project",
-                        "value": "proj"
-                    }
-                ]
-            },
-            {
-                "label": "Social/Fun",
-                "options": [
-                    {
-                        "label": "Social",
-                        "value": "social"
-                    },
-                ]
-            },
-            {
-            "label": " US Geo-Based Channels",
-            "options": [
-                    {
-                        "label": "Atlanta",
-                        "value": "atlanta"
-                    },
-                    {
-                        "label": "Austin",
-                        "value": "austin"
-                    },
-                    {
-                        "label": "Bellevue",
-                        "value": "bellevue"
-                    },
-                    {
-                        "label": "Boston",
-                        "value": "boston"
-                    },
-                    {
-                        "label": "Brooklyn",
-                        "value": "brooklyn"
-                    },
-                    {
-                        "label": "Budapest",
-                        "value": "Budapest"
-                    },
-                    {
-                        "label": "Chicago",
-                        "value": "chicago"
-                    },
-                    {
-                        "label": "Nashua",
-                        "value": "nashua"
-                    },
-                    {
-                        "label": "New York",
-                        "value": "new_york"
-                    },
-                    {
-                        "label": "Palo Alto",
-                        "value": "palo_alto"
-                    },
-                    {
-                        "label": "Raleigh",
-                        "value": "raleigh"
-                    },
-                    {
-                        "label": "San Fransico",
-                        "value": "sf"
-                    },
-                    {
-                        "label": "Tysons",
-                        "value": "tysons"
-                    },
-            ]
-        },
-        {
-            "label": " International Geo-Based Channels",
-            "options": [
-                    {
-                        "label": "Australia- Melbourne",
-                        "value": "melbourne"
-                    },
-                    {
-                        "label": "Australia- Sydney",
-                        "value": "sydney"
-                    },
-                    {
-                        "label": "Brazil- Sao Paulo",
-                        "value": "sao_paulo"
-                    },
-                    {
-                        "label": "China- Beijing",
-                        "value": "beijing"
-                    },
-                    {
-                        "label": "China- Shanghai",
-                        "value": "shanghai"
-                    },
-                    {
-                        "label": "France - Paris",
-                        "value": "paris"
-                    },
-                    {
-                        "label": "Germany- München",
-                        "value": "munchen"
-                    },
-                    {
-                        "label": "Hungary- Budapest",
-                        "value": "budapset"
-                    },
-                    {
-                        "label": "India- Chennai",
-                        "value": "chennai"
-                    },
-                    {
-                        "label": "Japan- Tokyo",
-                        "value": "tokyo"
-                    },
-                    {
-                        "label": "Mexico- Mexico City",
-                        "value": "cdmx"
-                    },
-                    {
-                        "label": "Singapore- Singapore",
-                        "value": "sg"
-                    },
-                    {
-                        "label": "South Korea- Seoul",
-                        "value": "seoul"
-                    },
-                    {
-                        "label": "United Arab Emirates- Dubai",
-                        "value": "dubai"
-                    },
-                    {
-                        "label": "United Kingdom- London",
-                        "value": "london"
-                    },
-            ]
         },
   ]
           },
@@ -259,7 +96,7 @@ app.post('/command', (req, res) => {
             label: 'Channel Name',
             type: 'text',
             name: 'name',
-            value: text,
+            value: inputName,
             hint: 'Be sure to keep it short! Channel name + Prefix must be less than 22 characters.',
           },
         ],
@@ -287,12 +124,12 @@ app.post('/command', (req, res) => {
  */
 app.post('/interactive', (req, res) => {
   const body = JSON.parse(req.body.payload);
-  const name = body.submission.prefix + '_' + body.submission.name;
-  const proper = name.split(' ').join('_');
+  const name = body.submission.prefix + '_' + body.submission.name.trim();
+  const proper = name.split(' ').join('_').replace(/-/g,"_");
 
 
   // check that the verification token matches expected value
-  if (signature.isVerified(req)) {
+  if (signature.isVerified(req) && body.callback_id === 'submit-ticket') {
     debug(`Channel submission received: ${body.submission.trigger_id}`);
 
     // immediately respond with a empty 200 response to let
@@ -301,12 +138,12 @@ app.post('/interactive', (req, res) => {
 
     web.channels.create({name: proper})
       .then((res) => {
+        console.log('res',res);
         // `res` contains information about the posted message
-        console.log(proper);
         console.log('Channel Created: ', res.channel.name.replace(/-/g,"_"));
         console.log('User Initiated: ', body.user.id);
         console.log('Channel info: ', res.channel.id);
-        web.channels.invite({
+        return  web.channels.invite({
             channel: res.channel.id,
             user: body.user.id
           })
@@ -314,14 +151,72 @@ app.post('/interactive', (req, res) => {
             // `res` contains information about the channels
             return response;
           })
-          .catch(console.error);
+          .catch((err) => {
+            const errorMessage = err.data.error.replace(/_/g," ");
+            return    web.chat.postEphemeral({ channel: body.user.id, user: body.user.id,  text: '`Channel Error: '+errorMessage+ '` Please see #help for assistance', link_names:"true" })
+                .then((response) => {
+                  // `res` contains information about the channels
+                  return response;
+                })
+                .catch(console.error);
+                })
       })
-      .catch(console.error);
+      .catch((err) => {
+        const errorMessage = err.data.error.replace(/_/g," ");
+        return    web.chat.postEphemeral({ channel: body.user.id, user: body.user.id,  text: '`Channel Error: '+errorMessage+ '` Please message  #help for assistance', link_names:"true" })
+            .then((response) => {
+              // `res` contains information about the channels
+              return response;
+            })
+            .catch(console.error);
+            })
+      } else if (signature.isVerified(req) && body.callback_id === 'rename-channel') {
+    debug(`Channel submission received: ${body.submission.trigger_id}`);
+
+    // immediately respond with a empty 200 response to let
+    // Slack know the command was received
+    res.send('');
+
+    // Rename Slack Channels
+    web.conversations.rename({name: proper.trim(), channel: body.channel.id})
+      .then((res) => {
+        // `res` contains information about the posted message
+        // post to log
+        console.log('Channel renamed: ', res.channel.name.replace(/-/g,"_"));
+        console.log('User Initiated: ', body.user.id);
+        console.log('Channel info: ', res.channel.id);
+        //send rename to channel log
+        renameTicket.create(body.user.id, body.submission, body.channel.name, body.channel.id, body.action_ts)
+      })
+      // check errors from channels.create.
+      // message user with ephemeral post.
+      .catch((err) => {
+        // console.log('req.body.payload',req.body.payload);
+        const errorMessage = err.data.error.replace(/_/g," ");
+        return    web.chat.postEphemeral({ channel: body.user.id, user: body.user.id,  text: '`Rename Error: '+errorMessage+ '` Please message  #help for assistance', link_names:"true" })
+            .then((response) => {
+              // `res` contains information about the channels
+              return response;
+            })
+            .catch(console.error);
+            })
   } else {
     debug('Token mismatch');
     res.sendStatus(404);
   }
+  // check users group list for sales users
+  // web.usergroups.users.list({usergroup : 'xxxxxx'})
+  // .then((res) => {
+  //   if (res.users.includes(body.user.id)) {
+  //     console.log('users in group', res.users);
+  //     console.log('creator id', body.user.id);
+  //     ticket.create(body.user.id, body.submission)
+  //     console.log('Ticket should be created', body.submission);
+  //   }
+  // })
 });
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Channel Creator server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
+// const server = https.createServer(credentials, app);
+// server.listen(5001);
